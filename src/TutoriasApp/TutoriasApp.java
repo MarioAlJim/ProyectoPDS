89974036888e4f21c0033df320604883c4455291
Mario Alberto Jimenez Jimenez <90919789+MarioAlJim@users.noreply.github.com>
2022-05-17 18:30:20 -0500

    2da

--- src/TutoriasApp/TutoriasApp.java
+++ src/TutoriasApp/TutoriasApp.java
@@ -20,6 +20,7 @@ public class TutoriasApp extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        //mario
     }
 
     public static void main(String[] args) {
